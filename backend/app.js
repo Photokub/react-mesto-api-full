735ef5c82e88b84e8284916dfa372c19488eb8e9
Photokub <photokub@yandex.ru>
2023-01-22 20:38:13 +0300

    Refactor: правка обработки CORS

--- backend/app.js
+++ backend/app.js
@@ -43,15 +43,35 @@ const allowedCors = [
   'http://photokub.domainname.nomoredomains.club/sign-in'
 ];
 
-const corsOptions = {
-  origin: allowedCors,
-  optionSuccessStatus: 200,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
-  credentials: true,
-};
+app.use(function(req, res, next) {
+  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
+  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE"; // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
+  const requestHeaders = req.headers['access-control-request-headers'];
+  // проверяем, что источник запроса есть среди разрешённых
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
 
-app.use(cors(corsOptions));
+  // Если это предварительный запрос, добавляем нужные заголовки
+  if (method === 'OPTIONS') {
+    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+
+  next();
+});
+
+// const corsOptions = {
+//   origin: allowedCors,
+//   optionSuccessStatus: 200,
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
+//   credentials: true,
+// };
+//
+// app.use(cors(corsOptions));
 
 app.use(requestLogger);  // подключаем логгер запросов
 
