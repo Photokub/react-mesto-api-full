9fa8edc180b558045ac752a4ca72e2766559c09b
Photokub <photokub@yandex.ru>
2023-01-22 19:38:49 +0300

    Refactor: правка allowedHeaders

--- backend/app.js
+++ backend/app.js
@@ -44,10 +44,10 @@ const allowedCors = [
 ];
 
 const corsOptions = {
-  origin: "*",
+  origin: allowedCors,
   optionSuccessStatus: 200,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization', 'Cookie'],
+  allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
   credentials: true,
 };
 
