2bd1b87c508c2fdc0810169f38ade22c0ba497d7
Photokub <photokub@yandex.ru>
2023-01-24 00:57:42 +0300

    Refactor: обновление подключения mongoose

--- backend/app.js
+++ backend/app.js
@@ -61,9 +61,9 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-mongoose.connect('mongodb://localhost:27017/mestodb', () => {
-  console.log('Подключение базы mestodb');
-});
+// mongoose.connect('mongodb://localhost:27017/mestodb', () => {
+//   console.log('Подключение базы mestodb');
+// });
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', () => {
   console.log('Подключение базы mestodb');
