6dc5081e92fdbb16f530db513119f5ad69653a33
Photokub <photokub@yandex.ru>
2023-02-09 23:26:51 +0300

    Refactor: отключение браузерной валидации

--- frontend/src/components/PopupWithForm.js
+++ frontend/src/components/PopupWithForm.js
@@ -9,7 +9,7 @@ export function PopupWithForm({isOpen, onClose, name, title, btnText, children, 
             <div className={`popup__container popup__container-${name}`}>
                 <h3 className="popup__title">{title}</h3>
                 <button className="popup__close" type="button" onClick={onClose}/>
-                <form className="form" name={`${name}`} method="post" onSubmit={onSubmit}>
+                <form className="form" name={`${name}`} method="post" onSubmit={onSubmit} novalidate>
                     {children}
                     <button
                         className="form__save-btn"
