ecb9ea5ffdfe6c9432c1544254d0e2b871378b60
Photokub <photokub@yandex.ru>
2023-02-09 23:12:38 +0300

    Refactor: рефактор ошибки в createCard

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -6,17 +6,21 @@ const BadRequestErr = require('../errors/forbidden-err');
 
 const createCard = async (req, res, next) => {
   try {
-    const { name, link } = req.body;
+    const {name, link} = req.body;
     const ownerId = req.user._id
     const card = (await Card.create({name, link, owner: ownerId}));
     console.log(req.user)
     console.log(card)
     return res.status(201).send(card);
   } catch (err) {
-    if (err instanceof 'ValidationError') {
-      return next(new BadRequestErr('Ошибка валидации'));
+    // if (err.name === 'ValidationError') {
+    //   return next(new BadRequestErr('Ошибка валидации'));
+    // }
+    if (err instanceof BadRequestErr) {
+      throw next('Ошибка валидации');
     }
-    return next(err);
+    throw next(err);
+    //return next(err);
   }
 };
 
@@ -32,7 +36,7 @@ const getCards = async (req, res, next) => {
 const deleteCard = (req, res, next) => {
   const userId = req.user._id;
 
-  Card.findById({ _id: req.params._id })
+  Card.findById({_id: req.params._id})
     .then((card) => {
       if (!card) {
         throw new NotFoundError('Невозможно найти');
@@ -40,8 +44,8 @@ const deleteCard = (req, res, next) => {
       if (!card.owner.equals(userId)) {
         throw new ForbiddenError('Невозможно удалить');
       }
-      card.remove({ _id: req.params._id })
-        .then(() => res.send({ message: 'Карточка удалена' })).catch(next);
+      card.remove({_id: req.params._id})
+        .then(() => res.send({message: 'Карточка удалена'})).catch(next);
     })
     .catch((err) => {
       if (err.name === 'CastError') {
@@ -54,8 +58,8 @@ const deleteCard = (req, res, next) => {
 const putLike = (req, res, next) => {
   Card.findByIdAndUpdate(
     req.params._id,
-    { $addToSet: { likes: req.user._id } },
-    { new: true },
+    {$addToSet: {likes: req.user._id}},
+    {new: true},
   )
     .then((card) => {
       if (!card) {
@@ -65,7 +69,7 @@ const putLike = (req, res, next) => {
     })
     .catch((err) => {
       if (err.name === 'CastError') {
-        return next(new BadRequestErr({ message: 'Переданы некорректные данные для постановки/снятии лайка.' }));
+        return next(new BadRequestErr({message: 'Переданы некорректные данные для постановки/снятии лайка.'}));
       }
       return next(err);
     });
@@ -74,8 +78,8 @@ const putLike = (req, res, next) => {
 const deleteLike = (req, res, next) => {
   Card.findByIdAndUpdate(
     req.params._id,
-    { $pull: { likes: req.user._id } },
-    { new: true },
+    {$pull: {likes: req.user._id}},
+    {new: true},
   )
     .then((card) => {
       if (!card) {
@@ -85,7 +89,7 @@ const deleteLike = (req, res, next) => {
     })
     .catch((err) => {
       if (err.name === 'CastError') {
-        return next(new BadRequestErr({ message: 'Переданы некорректные данные для постановки/снятии лайка.' }));
+        return next(new BadRequestErr({message: 'Переданы некорректные данные для постановки/снятии лайка.'}));
       }
       return next(err);
     });
